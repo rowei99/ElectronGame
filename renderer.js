9281acaa7d1a3ed01af1e8eb149d199bdf0cbb0b
Rowan Evans <rowanevans@Green.local>
2017-04-10 11:04:17 -0400

    commit3

--- renderer.js
+++ renderer.js
@@ -1,61 +1,54 @@
-
 //const electron = require('electron')
 // $(This) file is requicreature3 by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in $(this) process.
+function card (cost){;
+    this.cost = cost;
+};
+var creature1 = new card(2);
+var creature2 = new card(1);
+var creature3 = new card(3);
+var hand = ["empty","empty","empty","empty","empty"]
+var table = ["empty","empty","empty","empty","empty"]
 
-
-
-
-var deck = ["creature1", "creature2", "creature2", "creature2", "creature2", "creature3", "creature2", "creature3", "creature3"]
-var shuffle = []
-var decklocation = 0
-var length = deck.length
-for (var i = 0; i < length; i++) {
-    var rand = Math.floor((Math.random() * deck.length))
-    shuffle.push(deck[rand])
-    deck.splice(rand, 1)
-}
+var mana = 10;
+var deck = ["creature1", "creature2", "creature2", "creature2", "creature2", "creature3", "creature2", "creature3", "creature3"];
+var shuffle = [];
+var decklocation = 0;
+var length = deck.length;
+for (var i = 0; i < length; i++) {;
+    var rand = Math.floor((Math.random() * deck.length));
+    shuffle.push(deck[rand]);
+    deck.splice(rand, 1);
+};
 var shuffle_length = shuffle.length
 $("#deck").click(function(){
     if(decklocation < shuffle_length){
-        if($("#card1").hasClass("empty")){
-            $("#card1").removeClass("empty")
-            $("#card1").addClass(shuffle[decklocation])
-            decklocation = decklocation + 1
-        }else if($("#card2").hasClass("empty")){
-            $("#card2").removeClass("empty")
-            $("#card2").addClass(shuffle[decklocation])
-            decklocation = decklocation + 1
-        }else if($("#card3").hasClass("empty")){
-            $("#card3").removeClass("empty")
-            $("#card3").addClass(shuffle[decklocation])
-            decklocation = decklocation + 1
-        }else if($("#card4").hasClass("empty")){
-            $("#card4").removeClass("empty")
-            $("#card4").addClass(shuffle[decklocation])
-            decklocation = decklocation + 1
-        }else if($("#card5").hasClass("empty")){
-            $("#card5").removeClass("empty")
-            $("#card5").addClass(shuffle[decklocation])
-            decklocation = decklocation + 1
+        var done = false
+        for(i = 1 ;i < 6 && done == false; i++){
+            if($("#card" + i).hasClass("empty")){
+                $("#card" + i).removeClass("empty")
+                $("#card" + i).addClass(shuffle[decklocation])
+                hand[i - 1] = shuffle[decklocation]
+                decklocation = decklocation + 1
+                done = true
+            }
         }
+    console.log(hand)
     }
 });
 $(".card").click(function(){
     var classlist = $(this).attr('class').split(/\s+/)
-    console.log(classlist)
+    //console.log(classlist)
     var transfer = classlist.indexOf("card")
     classlist.splice(transfer, 1)
-    console.log(classlist[0])
+    //console.log(classlist[0])
     var cardclass = classlist[0]
     var placenum = this.id.replace('card','')
+    hand[placenum] = "empty"
+    $("#played" + placenum).removeClass()
     $("#played" + placenum).addClass(cardclass)
-    $("#played" + placenum).removeClass("empty")
+    $("#played" + placenum).addClass("played")
     $(this).removeClass(cardclass)
     $(this).addClass("empty")
-});
-$("#go").click(function(){
-    $("#output").html("TESTING")
-    console.log('working')
 });
